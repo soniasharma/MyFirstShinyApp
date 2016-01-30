b310ddddac0cf3d27aff495290250ff6389bc640
soniasharma <soniasharma24@gmail.com>
2016-01-30 14:51:00 -0800

    modified file

--- ui.R
+++ ui.R
@@ -38,16 +38,16 @@ shinyUI(fluidPage(
         tabPanel("Plot", plotOutput("plot")),
        
         tabPanel("Documentation",
-                h5("This shiny app helps with the visulization and understanding of some of the                    basic aspects of the mtcars dataset. There are four tabs:"),
+                h5("This shiny app helps with the visulization and understanding of some of the basic aspects of the mtcars dataset. There are four tabs:"),
                 
                 h4("Data Tab"),
-                "Set a number on the slider to see a given number of observations in the data. The                    default number of observations shown is 7.",
+                "Set a number on the slider to see a given number of observations in the data. The default number of observations shown is 7.",
                 
                 h4("Summary Tab"),
-                "Select a variable in the left hand side panel to see the type and the statistical                  summary of that variable.",
+                "Select a variable in the left hand side panel to see the type and the statistical summary of that variable.",
                 
                 h4("Plot Tab"),
-                "Select values for the x and the y variables to see the plot. Depending upon the                   type of variables selected we get a scatterplot, box plots or bar plots.",
+                "Select values for the x and the y variables to see the plot. Depending upon the type of variables selected we get a scatterplot, box plots or bar plots.",
                 
                 h4("Documentation Tab"),
                 "Users guide to using this shiny app. Hope you enjoyed playing with it!"
